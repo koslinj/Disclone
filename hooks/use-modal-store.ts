9d9ce12e854c33f203a7ec1d841332b3a95bffcc
koslinj <koslinj@gmail.com>
2023-10-01 18:02:20 +0200

    add data prop into modal hook

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,17 +1,24 @@
+import { Server } from "@prisma/client/edge";
 import { create } from "zustand"
 
-export type ModalType = "createServer";
+export type ModalType = "createServer" | "invite";
+
+interface ModalData {
+  server?: Server
+}
 
 interface ModalStore {
   type: ModalType | null
+  data: ModalData
   isOpen: boolean
-  onOpen: (type: ModalType) => void
+  onOpen: (type: ModalType, data?: ModalData) => void
   onClose: () => void
 }
 
 export const useModal = create<ModalStore>((set) => ({
   type: null,
+  data: {},
   isOpen: false,
-  onOpen: (type) => set({ isOpen: true, type: type }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type: type, data: data}),
   onClose: () => set({ type: null, isOpen: false })
 }))
