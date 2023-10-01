73aef64e330447a6eebd09e44e26e7ae65b851ad
koslinj <koslinj@gmail.com>
2023-10-01 18:17:22 +0200

    initial design for invite to server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -4,6 +4,7 @@ import { ServerWithMembersWithProfiles } from "@/types"
 import { MemberRole } from "@prisma/client/edge"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react"
+import { useModal } from "@/hooks/use-modal-store"
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles
@@ -11,6 +12,8 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal()
+
   const isAdmin = role === MemberRole.ADMIN
   const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -32,6 +35,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       >
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", {server: server})}
             className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
           >
             Invite People
