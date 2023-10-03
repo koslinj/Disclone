3c597424c7e42cbb3ef9179be95773715367ea97
koslinj <koslinj@gmail.com>
2023-10-03 18:24:12 +0200

    add manage members modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -53,6 +53,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("members", { server: server })}
             className="px-3 py-2 text-sm cursor-pointer"
           >
             Manage Members
