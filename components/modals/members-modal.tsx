752cd2f042e73ca531399662b71535c8e9c90f4f
koslinj <koslinj@gmail.com>
2023-10-03 19:13:32 +0200

    kick from server function in modal

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -27,6 +27,27 @@ export const MembersModal = () => {
   const isModalOpen = isOpen && type === "members"
   const { server } = data as { server: ServerWithMembersWithProfiles }
 
+  const onKick = async (memberId: string) => {
+    try {
+      setLoadingId(memberId)
+      const url = qs.stringifyUrl({
+        url: `/api/members/${memberId}`,
+        query: {
+          serverId: server?.id
+        }
+      })
+
+      const response = await axios.delete(url)
+
+      router.refresh()
+      onOpen("members", { server: response.data })
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoadingId("")
+    }
+  }
+
   const onRoleChange = async (memberId: string, role: MemberRole) => {
     try {
       setLoadingId(memberId)
@@ -40,7 +61,7 @@ export const MembersModal = () => {
       const response = await axios.patch(url, { role })
 
       router.refresh()
-      onOpen("members", {server: response.data})
+      onOpen("members", { server: response.data })
     } catch (error) {
       console.log(error)
     } finally {
@@ -104,7 +125,7 @@ export const MembersModal = () => {
                         </DropdownMenuPortal>
                       </DropdownMenuSub>
                       <DropdownMenuSeparator />
-                      <DropdownMenuItem>
+                      <DropdownMenuItem onClick={() => onKick(member.id)}>
                         <Gavel className="h-4 w-4 mr-2" />
                         Kick
                       </DropdownMenuItem>
