d6495848b3946f98f1d677ba00eccf1c96e5ea96
koslinj <koslinj@gmail.com>
2023-10-09 17:55:44 +0200

    add leave server modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -82,6 +82,7 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
         )}
         {!isAdmin && (
           <DropdownMenuItem
+            onClick={() => onOpen("leaveServer", { server })}
             className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Leave Server
