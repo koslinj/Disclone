e5b5206ef27f5a61023bd73831059f964dcbb146
koslinj <koslinj@gmail.com>
2024-02-13 22:40:56 +0100

    fix missing import

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Member, Profile } from "@prisma/client/edge"
+import { Member, Message, Profile } from "@prisma/client/edge"
 import { ChatWelcome } from "./chat-welcome"
 import { useChatQuery } from "@/hooks/use-chat-query"
 import { Loader2, ServerCrash } from "lucide-react"
